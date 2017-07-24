f495b1539f3392057bbd71b47029783b1452a4b7
Özgür Senekci <ozgursenekci@gmail.com>
2017-07-24 16:37:46 +0300

    LineCounter tests are written

--- tests/lineCounterTest.js
+++ tests/lineCounterTest.js
@@ -5,6 +5,8 @@
 var assert = require('assert');
 var fs = require("fs");
 var path = require("path");
+var LineCounter = require("../lib/LineCounter");
+var ExtensionsFactory = require("../lib/Extensions");
 
 describe("LineCounter", function(){
 
@@ -19,6 +21,64 @@ describe("LineCounter", function(){
         fs.writeFileSync(path.join(dir, "dir", "dir2", "file4.swift"), "line1\nline2");
         fs.writeFileSync(path.join(dir, "dir", "dir3", "file5.java"), "line1\nline2");
         fs.writeFileSync(path.join(dir, "dir", "dir3", "file6.js"), "line1\nline2");
+    });
+
+    describe("#resolveTargetFiles()", function(){
+        it("should return allowed extensions", function(){
+            var lc = new LineCounter();
+            lc.setPath(path.join(__dirname, "dir"));
+            lc.setExtensions(ExtensionsFactory.from("js"));
+            var result = lc.resolveTargetFiles();
+            var expected = [ path.join(__dirname, "dir", "dir3", "file6.js"), path.join(__dirname, "dir", "file1.js") ];
+            for( var i = 0; i < expected.length; i++ ){
+                assert.equal(expected[i], result[i].getPath());
+            }
+        });
+
+        it("should return all except disallowed ones", function(){
+            var lc = new LineCounter();
+            lc.setPath(path.join(__dirname, "dir"));
+            lc.setExtensions(ExtensionsFactory.except("java, swift, php"));
+            var result = lc.resolveTargetFiles();
+            var expected = [ path.join(__dirname, "dir", "dir3", "file6.js"), path.join(__dirname, "dir", "file1.js") ];
+            for( var i = 0; i < expected.length; i++ ){
+                assert.equal(expected[i], result[i].getPath());
+            }
+        });
+
+        it("should return all", function(){
+            var lc = new LineCounter();
+            lc.setPath(path.join(__dirname, "dir"));
+            var result = lc.resolveTargetFiles();
+            var expected = [ path.join(__dirname, "dir", "dir2", "file3.php"), path.join(__dirname, "dir", "dir2", "file4.swift"),
+                path.join(__dirname, "dir", "dir3", "file5.java"), path.join(__dirname, "dir", "dir3", "file6.js"),
+                path.join(__dirname, "dir", "file1.java"), path.join(__dirname, "dir", "file1.js")];
+            for( var i = 0; i < expected.length; i++ ){
+                assert.equal(expected[i], result[i].getPath());
+            }
+        });
+    });
+
+    describe("#getLines()", function(){
+        it("should count all files correctly", function(done){
+            var lc = new LineCounter();
+            lc.setPath(path.join(__dirname, "dir"));
+            lc.getLines(function(result){
+                assert.equal(6, result.files);
+                assert.equal(13, result.lines);
+                done();
+            });
+        });
+        it("should count only js files", function(done){
+            var lc = new LineCounter();
+            lc.setPath(path.join(__dirname, "dir"));
+            lc.setExtensions(ExtensionsFactory.from("js"));
+            lc.getLines(function(result){
+                assert.equal(2, result.files);
+                assert.equal(5, result.lines);
+                done();
+            });
+        });
     });
 
     after(function(){
