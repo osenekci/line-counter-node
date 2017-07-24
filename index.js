13427688ae346cd7fca48f088e852a18a7c2b7a4
Özgür Senekci <ozgursenekci@gmail.com>
2017-07-24 16:38:01 +0300

    index module is written

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
  * Created by ozgur on 24.07.2017.
  */
 
+var ExtensionsFactory = require("./lib/Extensions");
+var LineCounter = require("./lib/LineCounter");
+
+module.exports = {
+    LineCounter: LineCounter,
+    ExtensionsFactory: ExtensionsFactory
+};
